37cada5c151a2b4eb92dfee2390bab82fd07f1b9
leoses <leoses@ucm.es>
2019-12-18 14:58:45 +0100

    Al final, la cámara es fija y no se mueve

--- docs/Scripts/Enemies/SecurityCamera.js
+++ docs/Scripts/Enemies/SecurityCamera.js
@@ -5,19 +5,19 @@ export default class SecurityCamera extends EnemyWithVisionTrigger{
         super(scene, x, y, EnemyType, miNinja, 0, 0, 40, 200, VisionTrigger);
         
         this.visionTrigger.setOrigin(0.25,-0.25);
-        this.visionTrigger.angle = -60;
-        
+        //his.visionTrigger.angle = -60;
+        /*
         //Angulos entre los que se va a mover
         this.maxrotation = 10;
         this.minrotation = -this.maxrotation;
         this.speed = 10;
-        this.body.angularVelocity = this.speed;
+        this.body.angularVelocity = this.speed;*/
         
     }
-
+/*
     preUpdate(){
         
         if(this.angle> this.maxrotation)this.body.angularVelocity = -this.speed;
         else if(this.angle <this.minrotation) this.body.angularVelocity = this.speed;
-    }
+    }*/
 }
