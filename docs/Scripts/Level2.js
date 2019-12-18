4e15052c623bb143f700df059060a525abe3772c
leoses <leoses@ucm.es>
2019-12-18 14:50:40 +0100

    Debido al bug que hay al cambiar de nivel, dejamos de momento jugable solo el nivel 1
    Para la presentación del Hito 3 de hoy

--- docs/Scripts/Level2.js
+++ docs/Scripts/Level2.js
@@ -3,7 +3,7 @@ import LevelScene from './LevelScene.js';
 export default class Level2 extends LevelScene {
 
     constructor() {
-        super('./resources/maps/Level2.json', 'Level2', 150, 1000, 3, './resources/maps/backgrounds/background2.png', 'MainMenu');
+        super('./resources/maps/Level2.json', 'Level2', 150, 2000, 3, './resources/maps/backgrounds/background2.png', 'MainMenu');
     }
     
 }
